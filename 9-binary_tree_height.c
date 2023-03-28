661983dbebcec4f1296d9d4d9aa3b266a89d49e2
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-03-28 20:32:28 +0300

    Added 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,27 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t height = 0;
 
-	if (tree)
+	if (tree == NULL)
 	{
-		left_height = 1 +  binary_tree_height(tree->left);
-		right_height = 1 +  binary_tree_height(tree->right);
-		return ((left_height > right_height ? left_height : right_height));
+		return (height);
 	}
-	return (0);
+
+	const binary_tree_t *curr_node = tree;
+
+	while (curr_node != NULL)
+	{
+		height++;
+
+
+		size_t left_height = binary_tree_height(curr_node->left);
+		size_t right_height = binary_tree_height(curr_node->right);
+
+		if (left_height > right_height)
+			curr_node = curr_node->left;
+		else
+			curr_node = curr_node->right;
+	}
+	return (height - 1);
 }
